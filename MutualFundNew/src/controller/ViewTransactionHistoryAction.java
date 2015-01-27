622aeec6a108fd0020cff8062e7a4e5dd1a08ab9
mattyangk <kany@andrew.cmu.edu>
2015-01-27 18:39:07 -0500

    transaction history with price

--- MutualFundNew/src/controller/ViewTransactionHistoryAction.java
+++ MutualFundNew/src/controller/ViewTransactionHistoryAction.java
@@ -9,19 +9,29 @@ import javax.servlet.http.HttpSession;
 import org.genericdao.RollbackException;
 
 import databeans.CustomerBean;
+import databeans.FundBean;
+import databeans.FundPriceDetailBean;
+import databeans.FundPriceHistoryBean;
+import databeans.TransactionAndPriceBean;
 import databeans.TransactionBean;
 import model.CustomerDAO;
+import model.FundDAO;
+import model.FundPriceHistoryDAO;
 import model.Model;
 import model.TransactionDAO;
 
 public class ViewTransactionHistoryAction extends Action {
 
-	CustomerDAO customerDAO;
-	TransactionDAO transactionDAO;
+	private CustomerDAO customerDAO;
+	private TransactionDAO transactionDAO;
+	private FundDAO fundDAO;
+	private FundPriceHistoryDAO fundPriceHistoryDAO;
 
 	public ViewTransactionHistoryAction(Model model) {
 		customerDAO = model.getCustomerDAO();
 		transactionDAO = model.getTransactionDAO();
+		fundDAO = model.getFundDAO();
+		fundPriceHistoryDAO = model.getFundPriceHistoryDAO();
 	}
 
 	@Override
@@ -32,22 +42,59 @@ public class ViewTransactionHistoryAction extends Action {
 	@Override
 	public String perform(HttpServletRequest request) {
 		List<String> errors = new ArrayList<String>();
-		request.setAttribute("errors",errors);
+		request.setAttribute("errors", errors);
 
 		HttpSession session = request.getSession();
-		CustomerBean customer = (CustomerBean)session.getAttribute("customer");
+		CustomerBean customer = (CustomerBean) session.getAttribute("customer");
 		TransactionBean[] allTransactions = null;
+		TransactionAndPriceBean[] allTransactionWithPrices = null;
+		try {
+			allTransactions = transactionDAO
+					.getTransactionsByCustomerId(customer.getCustomer_id());
 
-		try{
-			allTransactions = transactionDAO.getTransactionsByCustomerId(customer.getCustomer_id());
-
-			if(allTransactions==null){
+			if (allTransactions == null) {
 				errors.add("No Transactions !");
 				return "viewTransactionsHistory.jsp";
+			} else {
+				allTransactionWithPrices = new TransactionAndPriceBean[allTransactions.length];
+				for (int i = 0; i < allTransactions.length; i++) {
+					FundBean fund = fundDAO.read(allTransactions[i]
+							.getFund_id());
+					TransactionAndPriceBean transactionWithPrice = new TransactionAndPriceBean();
+					transactionWithPrice.setAmount(allTransactions[i]
+							.getAmount());
+					transactionWithPrice.setCustomer_id(allTransactions[i]
+							.getCustomer_id());
+					transactionWithPrice.setExecute_date(allTransactions[i]
+							.getExecute_date());
+					transactionWithPrice.setFund_id(allTransactions[i]
+							.getFund_id());
+					transactionWithPrice.setFund_name(fund.getName());
+					transactionWithPrice.setIs_complete(allTransactions[i]
+							.isIs_complete());
+					transactionWithPrice.setIs_success(allTransactions[i]
+							.isIs_success());
+					transactionWithPrice.setShares(allTransactions[i]
+							.getShares());
+					transactionWithPrice.setTransaction_date(allTransactions[i]
+							.getTransaction_date());
+					transactionWithPrice.setTransaction_id(allTransactions[i]
+							.getTransaction_id());
+					transactionWithPrice.setTrasaction_type(allTransactions[i]
+							.getTrasaction_type());
+					if (allTransactions[i].getExecute_date() != null) {
+						FundPriceHistoryBean price = fundPriceHistoryDAO.read(
+								allTransactions[i].getFund_id(),
+								allTransactions[i].getExecute_date());
+						transactionWithPrice.setPrice(price.getPrice());
+					}
+					allTransactionWithPrices[i] = transactionWithPrice;
+				}
 			}
-			request.setAttribute("transactionsHistory", allTransactions);
+			request.setAttribute("transactionsHistory",
+					allTransactionWithPrices);
 
-		}catch (RollbackException e) {
+		} catch (RollbackException e) {
 			errors.add(e.getMessage());
 			return "manage.jsp";
 		}
