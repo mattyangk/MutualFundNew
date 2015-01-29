fb277a04630f402e9521d232e10cf5be47ec40f2
mattyangk <kany@andrew.cmu.edu>
2015-01-28 20:14:32 -0500

    employee view customer

--- MutualFundNew/src/controller/EmployeeViewCustomerAction.java
+++ MutualFundNew/src/controller/EmployeeViewCustomerAction.java
@@ -1,12 +1,16 @@
 package controller;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.genericdao.RollbackException;
 
+import util.ConvertUtil;
+import model.FundPriceHistoryDAO;
 import model.Model;
 import model.CustomerDAO;
 import model.PositionDAO;
@@ -17,11 +21,13 @@ public class EmployeeViewCustomerAction  extends Action{
 	CustomerDAO customerDAO;
 	PositionDAO positionDAO;
 	FundDAO fundDAO;
+	FundPriceHistoryDAO fundPriceHistoryDAO;
 	
 	public EmployeeViewCustomerAction(Model model){
 		customerDAO = model.getCustomerDAO();
 		positionDAO = model.getPositionDAO(); 
 		fundDAO = model.getFundDAO();
+		fundPriceHistoryDAO = model.getFundPriceHistoryDAO();
 	}
 	
 	public String getName() {
@@ -44,16 +50,19 @@ public class EmployeeViewCustomerAction  extends Action{
 		    
 			int CustomerID=theCustomer.getCustomer_id();
 			PositionBean [] Positions=positionDAO.getPositionsByCustomerId(CustomerID);
-			
 						
 			request.setAttribute("customer",theCustomer);
 			
-			if(Positions==null)
+			Date latestDay = fundPriceHistoryDAO.findLatestDate();			
+			request.setAttribute("latestDay", latestDay);
+			
+			if(Positions==null || latestDay == null)
 			{
 				messages="The customer does not have any fund yet";
 				request.setAttribute("message", messages);
 				return "viewAccountCustomer.jsp";
 			}
+			
 			
 			else{
 				CustomerFundsInfoBean [] fundInfo=new CustomerFundsInfoBean[Positions.length];
@@ -62,12 +71,18 @@ public class EmployeeViewCustomerAction  extends Action{
 				{
 					FundBean theFund=fundDAO.getFundById(Positions[i].getFund_id());
 					CustomerFundsInfoBean customerFundInfo = new CustomerFundsInfoBean();
+					FundPriceHistoryBean price = fundPriceHistoryDAO.read(Positions[i].getFund_id(), latestDay);
 					fundInfo[i] = customerFundInfo;
 					fundInfo[i].setFund_id(theFund.getFund_id());
 					fundInfo[i].setFund_name(theFund.getName());
 					fundInfo[i].setFund_symbol(theFund.getSymbol());
 					fundInfo[i].setShares(Positions[i].getShares());
 					fundInfo[i].setAvailable_shares(Positions[i].getAvailable_shares());
+					fundInfo[i].setPrice(price.getPrice());
+					double total = ConvertUtil.convertAmountLongToDouble(fundInfo[i].getPrice()) * ConvertUtil.convertShareLongToDouble(fundInfo[i].getShares());
+					total = new BigDecimal(total).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+					fundInfo[i].setTotal(ConvertUtil.convertAmountDoubleToLong(total));
+			
 				}
 				
 				request.setAttribute("fundInfo",fundInfo);
